6c232427eb0b8c6fca59b02be0bd401501da4051
Pedro Petrilli <ppetrilli96@gmail.com>
2024-08-01 16:07:19 -0300

    feat: add folder components and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
 const App = () => {
   return (
