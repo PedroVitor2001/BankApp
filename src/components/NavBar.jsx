ff3cea2585ec41de0a5cc84713e492c07b1ef5c6
Pedro Petrilli <ppetrilli96@gmail.com>
2024-08-01 20:30:41 -0300

    feat: add responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const NavBar = () => {
+  const [toggle, settoggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="bank" className="w-[124px] h-[32px]" />
@@ -19,6 +21,34 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt=""
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => settoggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-lg sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#$(nav.id)`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
